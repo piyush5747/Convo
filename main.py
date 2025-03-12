d2a7345639009a5a131fef935d791fea9fde8fec
piyush5747 <py848344@gmail.com>
2025-03-12 20:40:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>KataBump</title>
+
+                    <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+            <meta name="csrf-token" content="IZjPk0OvfI1kYfVBLHkIXwtINnOte9u3HTqHmtxx">
+            
+            <!-- meta data -->
+
+            <meta name="theme-color" content="#4a35cf"/>
+            <link rel="icon" type="image/x-icon" href="https://katabump.com/assets/favicon.png">
+
+            <meta name="title" content="KataBump Panel" />
+            <meta name="description" content="Gérez vos serveurs grâce au KataPanel" />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://control.katabump.com" />
+            <meta property="og:title" content="KataBump Panel" />
+            <meta property="og:description" content="Gérez vos serveurs grâce au KataPanel" />
+            <meta property="og:image" content="https://katabump.com/assets/favicon.png" />
+
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://control.katabump.com" />
+            <meta property="twitter:title" content="KataBump Panel" />
+            <meta property="twitter:description" content="Gérez vos serveurs grâce au KataPanel" />
+            <meta property="twitter:image" content="https://katabump.com/assets/favicon.png" />
+
+            <!-- meta data -->
+            <!--
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png?v=fb8d5b21c1cbde68eb758df7557d52e8">
+            <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+            <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+            <link rel="manifest" href="/favicons/manifest.json">
+            <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
+            <link rel="shortcut icon" href="/favicons/favicon.ico">
+            <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+        -->
+        
+                                    <script>
+                    window.PterodactylUser = {"uuid":"c93ea0b3-8d95-46d0-a6f7-21834bce3598","username":"b8b14413fb7e73b","email":"b8b14413fb7e73b@katabump.com","name_first":"Anas Khan","name_last":"Anas Khan","language":"en","root_admin":false,"use_totp":false,"gravatar":true,"created_at":"2025-03-12T07:35:00.000000Z","updated_at":"2025-03-12T07:35:00.000000Z"};
+                </script>
+                                        <script>
+                    window.SiteConfiguration = {"name":"KataBump","arix":{"logo":"https:\/\/katabump.com\/assets\/favicon.png","fullLogo":"false","logoHeight":"32px","discord":"1127906361246756967","support":"https:\/\/docs.katabump.com","status":"https:\/\/status.katabump.com\/","billing":"https:\/\/dashboard.katabump.com","announcementType":"disabled","announcementCloseable":"false","announcementMessage":"Need help with your hosting? Open a ticket on the support server.","mail_color":"#4a35cf","mail_backgroundColor":"#f5f5ff","mail_logo":"https:\/\/arix.gg\/arix.png","mail_logoFull":"false","mail_mode":"light","mail_discord":"1127906361246756967","mail_twitter":"https:\/\/x.com","mail_facebook":"https:\/\/facebook.com","mail_instagram":"https:\/\/instagram.com","mail_linkedin":"https:\/\/linkedin.com","mail_youtube":"https:\/\/youtube.com","mail_status":"https:\/\/arix.gg\/status","mail_billing":"https:\/\/arix.gg\/billing","mail_support":"https:\/\/arix.gg\/support","serverRow":"1","socialButtons":"true","discordBox":"false","statsCards":"2","sideGraphs":"1","graphs":"3","slot1":"banner","slot2":"statCards","slot3":"infoAdvanced","slot4":"info","slot5":"disabled","slot6":"disabled","slot7":"disabled","layout":"2","searchComponent":"2","logoPosition":"1","socialPosition":"1","loginLayout":"3","backgroundImage":"","backgroundImageLight":"","backdrop":"false","backdropPercentage":"100%","defaultMode":"darkmode","copyright":"Designed by KataBump","radiusInput":"7px","borderInput":"true","radiusBox":"10px","flashMessage":"2","pageTitle":"false","loginBackground":"\/arix\/background-login.png","loginGradient":"false","meta_color":"#4a35cf","meta_title":"KataBump Panel","meta_description":"G\u00e9rez vos serveurs gr\u00e2ce au KataPanel","meta_image":"https:\/\/katabump.com\/assets\/favicon.png","meta_favicon":"https:\/\/katabump.com\/assets\/favicon.png","font":"Roboto","profileType":"gravatar","modeToggler":"false","langSwitch":"true","ipFlag":"false","lowResourcesAlert":"false","primary":"#e6421f","successText":"#e1ffd8","successBorder":"#56aa2b","successBackground":"#3d8f1f","dangerText":"#ffd8d8","dangerBorder":"#aa2a2a","dangerBackground":"#8f1f20","secondaryText":"#b2b2c1","secondaryBorder":"#42425b","secondaryBackground":"#2b2b40","gray50":"#f4f4f4","gray100":"#d5d5db","gray200":"#b2b2c1","gray300":"#8282a4","gray400":"#5e5e7f","gray500":"#42425b","gray600":"#2b2b40","gray700":"#1d1d37","gray800":"#0b0d2a","gray900":"#040519","lightmode_primary":"#e6421f","lightmode_successText":"#e1ffd8","lightmode_successBorder":"#56aa2b","lightmode_successBackground":"#3d8f1f","lightmode_dangerText":"#ffd8d8","lightmode_dangerBorder":"#aa2a2a","lightmode_dangerBackground":"#8f1f20","lightmode_secondaryText":"#46464d","lightmode_secondaryBorder":"#c0c0d3","lightmode_secondaryBackground":"#000000","lightmode_gray50":"#141415","lightmode_gray100":"#27272c","lightmode_gray200":"#46464d","lightmode_gray300":"#626272","lightmode_gray400":"#757689","lightmode_gray500":"#a6a7bd","lightmode_gray600":"#000000","lightmode_gray700":"#e7e7ef","lightmode_gray800":"#ffffff","lightmode_gray900":"#ffffff"},"locale":"en","recaptcha":{"enabled":false,"siteKey":"6LcJcjwUAAAAAO_Xqjrtj9wWufUpYRnK6BW8lnfn"}};
+                </script>
+                            <style>
+            :root{
+                --borderInput: 1px solid;
+                --radiusBox: 10px;
+                --radiusInput: 7px;
+            }
+
+            :root{
+                --image: url();
+                --primary: #e6421f;
+
+                --successText: #e1ffd8;
+                --successBorder: #56aa2b;
+                --successBackground: #3d8f1f;
+
+                --dangerText: #ffd8d8;
+                --dangerBorder: #aa2a2a;
+                --dangerBackground: #8f1f20; 
+
+                --secondaryText: #b2b2c1;
+                --secondaryBorder: #42425b;
+                --secondaryBackground: #2b2b40;
+
+                --gray50: #f4f4f4;
+                --gray100: #d5d5db;
+                --gray200: #b2b2c1;
+                --gray300: #8282a4;
+                --gray400: #5e5e7f;
+                --gray500: #42425b;
+                --gray600: #2b2b40;
+                --gray700: color-mix(in srgb, #1d1d37 100%, transparent);
+                --gray800: #0b0d2a;
+                --gray900: #040519;
+
+                --gray700-default: #1d1d37;;
+            }
+            .lightmode{
+                --image: url();
+                --primary: #e6421f;
+
+                --successText: #e1ffd8;
+                --successBorder: #56aa2b;
+                --successBackground: #3d8f1f;
+
+                --dangerText: #ffd8d8;
+                --dangerBorder: #aa2a2a;
+                --dangerBackground: #8f1f20; 
+
+                --secondaryText: #46464d;
+                --secondaryBorder: #c0c0d3;
+                --secondaryBackground: #000000;
+
+                --gray50: #141415;
+                --gray100: #27272c;
+                --gray200: #46464d;
+                --gray300: #626272;
+                --gray400: #757689;
+                --gray500: #a6a7bd;
+                --gray600: #000000; 
+                --gray700: color-mix(in srgb, #e7e7ef 100%, transparent);
+                --gray800: #ffffff;
+                --gray900: #ffffff;
+
+                --gray700-default: #e7e7ef;;
+            }
+
+                        @import url('//fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap');
+            @import url('//fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:500&display=swap');
+        </style>
+
+        
+            </head>
+    <body class="bg-neutral-800">
+                                    <div id="modal-portal"></div>
+    <div id="app"></div>
+                                        <script src="/assets/bundle.bd8accec.js" crossorigin="anonymous"></script>
+        
+        <script>
+            localStorage.setItem("username", "thibaultl9834");
+            localStorage.setItem("BuyerID", "517758");
+            localStorage.setItem("Timestamp", "1732730748");
+    </script>
+
+    
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-89JJRB0SR9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-89JJRB0SR9');
+</script>
+    <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"91f1a7babe8f2d58","version":"2025.1.0","r":1,"token":"2e5da1eb3ccd4357b59d2877ea37b6bc","serverTiming":{"name":{"cfExtPri":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}}}' crossorigin="anonymous"></script>
+</body>
+</html>
